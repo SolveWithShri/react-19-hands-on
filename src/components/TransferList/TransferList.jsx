0a89598878d6b293459202bfc6213641a5f33e8d
Shrikant Adhav <shrikantadhav11@gmail.com>
2025-07-28 16:59:12 +0530

    TransferList

--- src/components/TransferList/TransferList.jsx
+++ src/components/TransferList/TransferList.jsx
@@ -1,15 +1,122 @@
+import { useState } from "react";
 import "./TransferList.css";
 
 const TransferList = () => {
+  const [leftList, setLeftList] = useState([
+    {
+      key: "angular",
+      Label: "Angular",
+      checked: true,
+    },
+    {
+      key: "solid",
+      Label: "Solid JS",
+      checked: false,
+    },
+    {
+      key: "vue",
+      Label: "Vue",
+      checked: false,
+    },
+  ]);
+  const [rightList, setRightList] = useState([
+    {
+      key: "svelte",
+      Label: "Svelte JS",
+      checked: false,
+    },
+    {
+      key: "react",
+      Label: "React",
+      checked: false,
+    },
+  ]);
+
+  const handleLeftListCheckboxChange = (il) => {
+    setLeftList((prevCheckboxes) =>
+      prevCheckboxes.map((item, i) =>
+        i === il ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const handleRightListCheckboxChange = (ir) => {
+    setRightList((prevCheckboxes) =>
+      prevCheckboxes.map((item, i) =>
+        i === ir ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
+
+  const leftToRightCheckedTransfer = () => {
+    const checkedList = [];
+    const unCheckedList = [];
+
+    leftList.forEach((ll) => {
+      if (ll.checked) {
+        checkedList.push(ll);
+      } else {
+        unCheckedList.push(ll);
+      }
+    });
+
+    setLeftList(unCheckedList);
+    setRightList((preList) => [
+      ...preList,
+      ...checkedList.map((cl) => ({ ...cl, checked: false })),
+    ]);
+
+    console.log({ leftList, rightList });
+  };
+
   return (
     <div className="transfer-list">
       <div className="transfer-list-header">
         Javascript Libraries and Frameworks
       </div>
       <div className="transfer-list-main">
-        <div className="left-list"></div>
-        <div className="listp-actions"></div>
-        <div className="right-list"></div>
+        <div className="left-list list-wrapper">
+          <ul>
+            {leftList.map((ll, il) => {
+              return (
+                <li key={`${ll.key}`}>
+                  {" "}
+                  <input
+                    type="checkbox"
+                    id={ll.key}
+                    checked={ll.checked}
+                    onChange={() => handleLeftListCheckboxChange(il)}
+                  />{" "}
+                  <label htmlFor={ll.key}>{ll.Label}</label>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="list-actions">
+          <button className="action" onClick={leftToRightCheckedTransfer}>{">"}</button>
+          <button className="action">{">>"}</button>
+          <button className="action">{"<"}</button>
+          <button className="action" >{"<<"}</button>
+        </div>
+        <div className="right-list list-wrapper">
+          <ul>
+            {rightList.map((ll, ir) => {
+              return (
+                <li key={ll.key}>
+                  {" "}
+                  <input
+                    type="checkbox"
+                    id={ll.key}
+                    checked={ll.checked}
+                    onChange={() => handleRightListCheckboxChange(ir)}
+                  />{" "}
+                  <label htmlFor={ll.key}>{ll.Label}</label>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
